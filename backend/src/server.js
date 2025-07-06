d0773a6118e4e59352bd88bf7ced14d289addb14
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2025-07-06 15:23:56 +0530

    Implement database connection and environment configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,16 @@
 import express from "express";
 
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
 const app = express();
 
-const startServer = async () => {
-//   try {
-    
-    // listen for local development
-    // if (ENV.NODE_ENV !== "production") {
-      app.listen(5000, () =>
-        console.log("Server is up and running on PORT:", 5000)
-      );
-    // }x
-  
-};
+app.get("/", (req, res) => res.send("Hello from server"));
 
-startServer();
+connectDB();
 
-// export for vercel
+app.listen(ENV.PORT, () =>
+  console.log("Server is up and running on PORT:", ENV.PORT)
+);
+
 export default app;
